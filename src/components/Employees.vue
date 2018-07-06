60a01e8794b266c4fde1404630c7d7763f58448f
Ivan <ivancernja@gmail.com>
2018-07-06 15:17:52 +0200

    added upload via button, multiple files

--- src/components/Employees.vue
+++ src/components/Employees.vue
@@ -4,65 +4,168 @@
 
   <h3>Employees</h3>
   <!-- TODO: Fetch from csv and put into employee.x -->
-    <div
-    v-for="employee in sortedEmployees"
-    v-bind:key="employee.id"
-    class="collection"
-    >
-
-    <div class="collection-item">
-    <table style="margin: 0 auto;">
-      <tbody>
+    <table style= "margin: 0 auto;">
+      <thead>
         <tr>
+          <th>
+            Name:
+          </th>
+          <th>
+            Hours:
+          </th>
+          <th>
+            Type:
+          </th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr
+          v-for="employee in sortedEmployees"
+          v-bind:key="employee.id"
+          class="collection"
+        >
           <td>
-            Name:<br>
-            {{employee.name}}</td>
+            {{employee.name}}
+          </td>
           <td>
-            Hours:<br>
-          {{employee.totalhours}}</td>
+          {{employee.totalhours}}
+          </td>
           <td>
-            Type:<br>
-          {{employee.type}}</td>
+          {{employee.type}}
+          </td>
         </tr>
       </tbody>
     </table>
-    </div>
 
-  </div>
-    <button type="button" class="btn btn-default">Import CSV</button>
+<!-- Enter name; Enter hours; Dropdown type -->
+    
+    <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
+        <h1>Upload CSV</h1>
+        <button type="button" class="btn btn-dark" v-on:click="fileUpload">Upload</button>
+          <input :ref="'inputField'" type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length" accept="image/*" class="input-file" hidden>
+            <p v-if="isInitial">
+              Upload TW CSV
+            </p>
+            <p v-if="isSaving">
+              Uploading {{ fileCount }} files...
+            </p>
+  
+      </form>
+       <div v-if="isSuccess">
+        <h2>Uploaded {{ uploadedFiles.length }} file(s) successfully.</h2>
+        <p>
+          <a href="javascript:void(0)" @click="reset()">Upload again</a>
+        </p>
+        <ul class="list-unstyled">
+          <li v-for="item in uploadedFiles">
+            <img :src="item.url" class="img-responsive img-thumbnail" :alt="item.originalName">
+          </li>
+        </ul>
+      </div>
+      <!--FAILED-->
+      <div v-if="isFailed">
+        <h2>Upload failed.</h2>
+        <p>
+          <a href="javascript:void(0)" @click="reset()">Try again</a>
+        </p>
+        <pre>{{ uploadError }}</pre>
+      </div>
+    </div>
 </div>
 </template>
 
 <script>
-import db from '@/db'
+    import db from '@/db'
+    import {upload} from '@/file-upload.service';
 
-export default {
-  name: 'Employees',
-  data() {
-    return {
-      employees: []
+    const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
+    
+
+    export default {
+      name: 'Employees',
+      data() {
+        return {
+          employees: [],
+          uploadedFiles: [],
+          uploadError: null,
+          currentStatus: null,
+          uploadFieldName: 'export'
+        }
+      },
+      computed: {
+        sortedEmployees() {
+          return this.employees.slice().sort((a, b) => {
+            return a.id - b.id
+          })
+        },
+        isInitial() {
+        return this.currentStatus == STATUS_INITIAL;
+        },
+        isSaving() {
+          return this.currentStatus == STATUS_SAVING;
+        },
+        isSucess() {
+          return this.currentStatus == STATUS_SUCCESS;
+        },
+        isFailed() {
+          return this.currentStatus == STATUS_FAILED;
+        }
+      },
+      created() {
+        db.collection('employees').get().then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            this.employees.push(doc.data())
+          })
+        })
+      },
+      methods: {
+      reset() {
+        // reset form
+        this.currentStatus = STATUS_INITIAL;
+        this.uploadedFiles = [];
+        this.uploadError = null;
+      },
+      save(formData) {
+        // upload data to the server
+        this.currentStatus = STATUS_SAVING;
+
+        upload(formData)
+          .then(x => {
+            this.uploadedFiles = [].concat(x);
+            this.currentStatus = STATUS_SUCCESS;
+          })
+          .catch(err => {
+            this.uploadError = err.response;
+            this.currentStatus = STATUS_FAILED
+          });
+      },
+      filesChange(fieldName, fileList) {
+        const formData = new FormData();
+
+        if (!fileList.length) return;
+
+        Array
+          .from(Array(fileList.length).keys())
+          .map(x => {
+            formData.append(fieldName, fileList[x], fileList[x].name);
+          });
+
+        this.save(formData);
+      },
+      fileUpload() {
+      this.$refs.inputField.click()
     }
-  },
-  computed: {
-    sortedEmployees() {
-      return this.employees.slice().sort((a, b) => {
-        return a.id - b.id
-      })
-    }
-  },
-  created() {
-    db.collection('employees').get().then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        this.employees.push(doc.data())
-      })
-    })
-  }
-}
+    },
+    mounted() {
+      this.reset();
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+      font-weight: normal;
+    }
+
 </style>
